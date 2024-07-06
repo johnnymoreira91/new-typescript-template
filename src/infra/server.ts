40ea30608769426919408943e3c22dae142bbbb3
Johnny Moreira <jmoreira@engebras.com.br>
2024-07-06 18:15:22 -0300

    fix all

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,6 +1,7 @@
 import express, { Application, json } from "express";
 import helmet from "helmet";
 import cors from "cors";
+import Logger from "@infra/service/logger/winston";
 
 const app: Application = express();
 
@@ -8,6 +9,22 @@ app.use(json());
 app.use(helmet());
 app.use(cors());
 
-export {
-  app
+let server; 
+
+const shutdown = (signal: string) => {
+  return (err?: Error) => {
+    if (err) Logger.error(`Erro recebido: ${err.message}`, err);
+
+    Logger.info(`Recebido sinal ${signal}. Fechando servidor...`);
+    
+    process.exit(err ? 1 : 0);
+  };
 };
+
+process.on('SIGTERM', shutdown('SIGTERM'));
+process.on('SIGINT', shutdown('SIGINT'));
+
+process.on('uncaughtException', shutdown('uncaughtException'));
+process.on('unhandledRejection', shutdown('unhandledRejection'));
+
+export { app };
